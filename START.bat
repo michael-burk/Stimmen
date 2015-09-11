95bddbaee83c03615ab2a2b0b990ec13c5d9eee9
Burk <michael.burk87@gmail.com>
2015-09-11 12:59:21 +0200

    Working on Database Queries

--- START.bat
+++ START.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-start C:\vvvv\vvvv_45beta33.3_x86\vvvv.exe /allowmultiple /o "C:\Users\Michael\Documents\GitHub\Stimmen\subpatches\sound\Playback.v4p"
+start C:\vvvv\vvvv_45beta34.1_x86\vvvv.exe /allowmultiple /o "C:\Users\Michael\Documents\GitHub\Stimmen\subpatches\sound\Playback.v4p"
 timeout 5
 start C:\vvvv\vvvv_45beta33.7_x64\vvvv.exe /allowmultiple /o "C:\Users\Michael\Documents\GitHub\Stimmen\main.v4p"
